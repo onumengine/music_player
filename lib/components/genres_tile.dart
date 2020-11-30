0511d3314e6f625882c3f4aa8e07233df54c3de4
onumengine <onumdev@gmail.com>
2020-11-30 13:52:43 +0100

    Fix(genres_tile.dart)Fixed the genres screen

--- lib/components/genres_tile.dart
+++ lib/components/genres_tile.dart
@@ -3,14 +3,33 @@ import 'package:flutter/material.dart';
 class GenresTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 100.0,
-      height: 30.0,
-      child: Stack(
-        alignment: AlignmentDirectional.bottomStart,
-        children: <Widget>[
-          Image.asset('lib/images/three.jpg'),
-          Text('Hip-hop'),
-        ],
+      width: double.infinity,
+      height: 100.0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+        child: Stack(
+          alignment: AlignmentDirectional.bottomStart,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.teal.shade100,
+                border: Border.all(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Hip-hop',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
