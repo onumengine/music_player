6645a10cfda331f8e7ea4508db60023348f01646
onumengine <onumdev@gmail.com>
2020-12-02 04:49:46 +0100

    Feat(artist_page.dart)Finished the [ArtistPage]

--- lib/ui/screens/library/artist_page.dart
+++ lib/ui/screens/library/artist_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/ui/screens/library/albums_screen.dart';
+import 'package:music_player/ui/screens/library/tracks_screen.dart';
 
 class ArtistPage extends StatefulWidget {
   ArtistPageState createState() => ArtistPageState();
@@ -6,8 +8,39 @@ class ArtistPage extends StatefulWidget {
 
 class ArtistPageState extends State<ArtistPage> {
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              automaticallyImplyLeading: false,
+              title: Text('Pyro The Rapper'),
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.asset('lib/images/map.jpg', fit: BoxFit.fill),
+              ),
+              bottom: TabBar(
+                tabs: <Tab>[
+                  Tab(text: 'ALBUMS'),
+                  Tab(text: 'BIO'),
+                  Tab(text: 'TRACKS'),
+                ],
+              ),
+            ),
+            SliverFillRemaining(
+              child: TabBarView(
+                children: <Widget>[
+                  AlbumsScreen(),
+                  Column(),
+                  TracksScreen(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
