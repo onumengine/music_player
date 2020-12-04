d5e49f2ea911aa40a3a775e680c18ede3c7490d2
onumengine <onumdev@gmail.com>
2020-12-04 09:50:15 +0100

    Feat(genre_page.dart)[AppBar] action now displays an [AlertDialog]

--- lib/ui/components/genres_tile.dart
+++ lib/ui/components/genres_tile.dart
@@ -2,35 +2,40 @@ import 'package:flutter/material.dart';
 
 class GenresTile extends StatelessWidget {
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 100.0,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 2.0),
-        child: Stack(
-          alignment: AlignmentDirectional.bottomStart,
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.teal.shade100,
-                border: Border.all(
-                  color: Colors.white,
+    return GestureDetector(
+      child: SizedBox(
+        width: double.infinity,
+        height: 100.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 2.0),
+          child: Stack(
+            alignment: AlignmentDirectional.bottomStart,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.teal.shade100,
+                  border: Border.all(
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Hip-hop',
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 20.0,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Hip-hop',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 20.0,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        Navigator.of(context).pushNamed('/genre_page');
+      },
     );
   }
 }
--- lib/ui/screens/library/genre_page.dart
+++ lib/ui/screens/library/genre_page.dart
@@ -1,16 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/ui/screens/library/albums_screen.dart';
+import 'package:music_player/ui/screens/library/artists_screen.dart';
+import 'package:music_player/ui/screens/library/tracks_screen.dart';
 
 class GenrePage extends StatefulWidget {
   GenrePageState createState() => GenrePageState();
 }
 
 class GenrePageState extends State<GenrePage> {
+  void displayDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Hip-Hop'),
+            scrollable: true,
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      print('YOU TAPPED PLAY');
+                    },
+                    child: Text(
+                      'Play',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      print('YOU TAPPED PLAY');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: Text(
+                        'Play Next',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      print('YOU TAPPED PLAY');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: Text(
+                        'Queue',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      print('YOU TAPPED PLAY');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: Text(
+                        'Add to Playlist',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      print('YOU TAPPED PLAY');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: Text(
+                        'Edit',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      print('YOU TAPPED PLAY');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: Text(
+                        'Add Tracks to Favorites',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            actions: [],
+          );
+        });
+  }
+
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
           appBar: AppBar(
             title: Text('Hip-hop'),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.menu_rounded),
+                onPressed: () {
+                  displayDialog();
+                },
+              ),
+            ],
             bottom: TabBar(
               tabs: <Tab>[
                 Tab(
@@ -26,7 +159,11 @@ class GenrePageState extends State<GenrePage> {
             ),
           ),
           body: TabBarView(
-            children: [],
+            children: [
+              TracksScreen(),
+              AlbumsScreen(),
+              ArtistsScreen(),
+            ],
           )),
     );
   }
