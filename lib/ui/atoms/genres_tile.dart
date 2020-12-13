2a6777d6c6d20e7b8c530d79f6b104bf52da9ab1
onumengine <onumdev@gmail.com>
2020-12-13 21:14:16 +0100

    Fix(genres_tile.dart):Removed the unnecessary SizedBox

--- lib/ui/atoms/genres_tile.dart
+++ lib/ui/atoms/genres_tile.dart
@@ -3,37 +3,31 @@ import 'package:flutter/material.dart';
 class GenresTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: SizedBox(
-        width: double.infinity,
-        height: 100.0,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 2.0),
-          child: Stack(
-            alignment: AlignmentDirectional.bottomStart,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.black87,
-                ),
-                child: Image.asset(
-                  'lib/assets/images/10.png',
-                  fit: BoxFit.none,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Hip-hop',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                  ),
-                ),
-              ),
-            ],
+      child: Stack(
+        alignment: AlignmentDirectional.bottomStart,
+        children: <Widget>[
+          Container(
+            height: 100.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.black87,
+            ),
+            child: Image.asset(
+              'lib/assets/images/10.png',
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Hip-hop',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+        ],
       ),
       onTap: () {
         Navigator.of(context).pushNamed('/genre_page');
