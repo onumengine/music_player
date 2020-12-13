a32f6545ddff33f3943ecb598b6ad65ec853fc89
onumengine <onumdev@gmail.com>
2020-12-13 16:49:30 +0100

    Task(album_tile.dart):Deleted a redundant comment

--- lib/ui/atoms/album_tile.dart
+++ lib/ui/atoms/album_tile.dart
@@ -47,43 +47,6 @@ class AlbumTile extends StatelessWidget {
           ),
         ],
       ),
-      /*
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Image.asset(this.imageUrl, fit: BoxFit.cover),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.purpleAccent,
-              ),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(
-                      this.albumTitle,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Text(this.artist),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ), */
     );
   }
 }
