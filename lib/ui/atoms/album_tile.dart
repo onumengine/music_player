6c4d459a9ca4751a52062e65dc975cf82315deb1
onumengine <onumdev@gmail.com>
2020-12-14 07:58:17 +0100

    Feat(album_tile.dart):Created a new [AlbumTile]

--- lib/ui/atoms/album_tile.dart
+++ lib/ui/atoms/album_tile.dart
@@ -13,6 +13,40 @@ class AlbumTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: this.onTap,
+      child: Stack(
+        children: [
+          Image.asset(this.imageUrl, fit: BoxFit.cover),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              color: Colors.purpleAccent,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        this.albumTitle,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                    ),
+                    Text(
+                      this.artist,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      /*
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
@@ -48,7 +82,7 @@ class AlbumTile extends StatelessWidget {
             ),
           ),
         ],
-      ),
+      ), */
     );
   }
 }
