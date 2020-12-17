4b1e40a5a2052c1dc7ac4f7918f5a3a2d83d3b66
onumengine <onumdev@gmail.com>
2020-12-17 10:15:16 +0100

    Task(route_generator.dart):Removed the unnecessary args variable

--- lib/ui/route_generator.dart
+++ lib/ui/route_generator.dart
@@ -11,8 +11,6 @@ import 'package:music_player/ui/pages/artist_page.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    final args = settings.arguments;
-
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(builder: (context) => MyHomePage());
