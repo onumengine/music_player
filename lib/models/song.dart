dae41ddb8f66bc9ef0d882c4c563d39674f950f0
onumengine <onumdev@gmail.com>
2021-01-25 22:22:36 +0100

    Feat(song.dart):Defined the [Song] class

---
+++ lib/models/song.dart
@@ -0,0 +1,11 @@
+class Song {
+  String title;
+  String artist;
+  String album;
+  int albumTrackNumber;
+  int discNumber;
+  String genre;
+  String filePath;
+
+  Song({this.title, this.artist, this.album, this.albumTrackNumber, this.discNumber, this.genre, this.filePath});
+}
