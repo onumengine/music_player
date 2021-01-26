cb1dde008ed4efd569b9d495107d1d7573d9fc64
onumengine <onumdev@gmail.com>
2021-01-26 10:22:03 +0100

    Feat(now_playing_page_bloc.dart):Integrated the [NowPlayingPageBloc]

--- lib/blocs/pages/now_playing_page_bloc.dart
+++ lib/blocs/pages/now_playing_page_bloc.dart
@@ -7,10 +7,66 @@ import 'package:music_player/util/strings.dart';
 class NowPlayingBloc extends ChangeNotifier {
   final _platformChannel = MethodChannel(METHOD_CHANNEL_MUSIC_LIBRARY);
 
+  String _currentlyPlayingTrackTitle = '<unknown>';
+  String get currentlyPlayingTrackTitle => _currentlyPlayingTrackTitle;
+  set currentlyPlayingTrackTitle(String currentTrack) {
+    _currentlyPlayingTrackTitle = currentTrack;
+    notifyListeners();
+  }
+
+  String _currentlyPlayingTrackArtist = '<unknown>';
+  String get currentlyPlayingTrackArtist => _currentlyPlayingTrackArtist;
+  set currentlyPlayingTrackArtist(String currentArtist) {
+    _currentlyPlayingTrackArtist = currentArtist;
+    notifyListeners();
+  }
+
+  String _currentlyPlayingTrackAlbum = '<unknown>';
+  String get currentlyPlayingTrackAlbum => _currentlyPlayingTrackAlbum;
+  set currentlyPlayingTrackAlbum(String currentAlbum) {
+    _currentlyPlayingTrackAlbum = currentAlbum;
+    notifyListeners();
+  }
+
+  Image _currentlyPlayingAlbumArt;
+  Image get currentlyPlayingAlbumArt => _currentlyPlayingAlbumArt;
+  set currentlyPlayingAlbumArt(Image currentImage) {
+    _currentlyPlayingAlbumArt = currentImage;
+    notifyListeners();
+  }
+
+  DateTime _currentlyPlayingTrackDuration;
+  DateTime get currentlyPlayingTrackDuration => _currentlyPlayingTrackDuration;
+  set currentlyPlayingTrackDuration(DateTime currentTrackDuration) {
+    _currentlyPlayingTrackDuration = currentTrackDuration;
+    notifyListeners();
+  }
+
+  DateTime _currentlyPlayingElapsedTime;
+  DateTime get currentlyPlayingElapsedTime => _currentlyPlayingElapsedTime;
+  set currentlyPlayingElapsedTime(DateTime elapsedTime) {
+    _currentlyPlayingElapsedTime = elapsedTime;
+    notifyListeners();
+  }
+
   double _trackProgress = 0.0;
   double get trackProgress => _trackProgress;
   set trackProgress(double value) {
     _trackProgress = value;
+    notifyListeners();
+  }
+
+  int _currentlyPlayingTrackIndex;
+  int get currentlyPlayingTrackIndex => _currentlyPlayingTrackIndex;
+  set currentlyPlayingTrackIndex(int currentIndex) {
+    _currentlyPlayingTrackIndex = currentIndex;
+    notifyListeners();
+  }
+
+  int _songsInCurrentPlaylist;
+  int get songsInCurrentPlaylist => _songsInCurrentPlaylist;
+  set songsInCurrentPlaylist(int totalSongs) {
+    _songsInCurrentPlaylist = totalSongs;
     notifyListeners();
   }
 
