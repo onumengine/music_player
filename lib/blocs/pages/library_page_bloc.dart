5aca634706b89c2bede7da4c9efcb8545e223df4
onumengine <onumdev@gmail.com>
2021-01-26 12:51:35 +0100

    Feat(library_page_bloc.dart):Created the LibraryPageBloc

--- lib/blocs/pages/library_page_bloc.dart
+++ lib/blocs/pages/library_page_bloc.dart
@@ -1,5 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/models/song.dart';
 
 class LibraryPageBloc extends ChangeNotifier {
-  
+
+  List<String> _allAlbums;
+  List<String> get allAlbums => _allAlbums;
+  set allAlbums(List<String> albums) {
+    _allAlbums = albums;
+    notifyListeners();
+  }
+
+  List<String> _allArtists;
+  List<String> get allArtists => _allArtists;
+  set allArtists(List<String> updatedArtists) {
+    _allArtists = updatedArtists;
+    notifyListeners();
+  }
+
+  List<String> _allGenres;
+  List<String> get allGenres => _allGenres;
+  set allGenres(List<String> genres) {
+    _allGenres = genres;
+    notifyListeners();
+  }
+
+  List<Song> _allTracksInLibrary;
+  List<Song> get allTracksInLibrary => _allTracksInLibrary;
+  set allTracksInLibrary(List<Song> trackList) {
+    _allTracksInLibrary = trackList;
+    notifyListeners();
+  }
+
+  List<Song> _searchResults;
+  List<Song> get searchResults => _searchResults;
+  set searcResults(List<Song> results) {
+    _searchResults = results;
+    notifyListeners();
+  }
+
+  refreshLibrary() {}
+
+  sortTracksAlphabetically() {}
+
+  sortTracksReverseAlphabetically() {}
+
+  sortTracksByDateAdded() {}
+
+  sortAlbumsAlphabetically() {}
+
+  sortAlbumsReverseAlphabetically() {}
+
+  sortArtistsAlphabetically() {}
+
+  sortArtistsReverseAlphabetically() {}
+
+  sortTracksByDuration() {}
+
+  sortTracksByFilePath() {}
+
+  sortTrackYearsInAscendingOrder() {}
+
+  sortTrackYearsInDescendingOrder() {}
 }
