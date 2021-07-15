4351f788ac8a987f23fb2c97e5b9fd91572c01be
onumdev <onumdev@gmail.com>
2021-07-15 13:18:02 +0100

    add an artist fetch method to the repository

--- lib/repository/music_repo.dart
+++ lib/repository/music_repo.dart
@@ -2,9 +2,8 @@ import 'package:flutter_audio_query/flutter_audio_query.dart';
 
 class MusicRepository {
   static Future<List<SongInfo>> fetchSongsInDevice() async {
-    final FlutterAudioQuery flutterAudioQuery = FlutterAudioQuery();
     try {
-      List<SongInfo> allSongsInDevice = await flutterAudioQuery.getSongs();
+      List<SongInfo> allSongsInDevice = await FlutterAudioQuery().getSongs();
       print("ABOUT TO PRINT ALL SONGS");
       print(allSongsInDevice);
       print("ALL ${allSongsInDevice.length} SONGS PRINTED SUCCESSFULLY");
@@ -12,6 +11,16 @@ class MusicRepository {
       return allSongsInDevice;
     } on Exception catch (e) {
       print("MUSIC FETCH ENDED WITH AN EXCEPTION: \n$e");
+    }
+  }
+
+  static Future<List<ArtistInfo>> fetchArtistsInDevice() async {
+    try {
+      List<ArtistInfo> allArtistsInDevice =
+          await FlutterAudioQuery().getArtists();
+      return allArtistsInDevice;
+    } on Exception catch (e) {
+      print("ARTIST FETCH ENDED WITH THIS EXCEPTION: \n$e");
     }
   }
 }
