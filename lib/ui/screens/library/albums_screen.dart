87a37000d1d6c0f2a436776bcf2e9db00e1fb5e4
onumdev <onumdev@gmail.com>
2021-07-18 10:30:32 +0100

    convert the AlbumsScreen to a stateless widget

--- lib/ui/screens/library/albums_screen.dart
+++ lib/ui/screens/library/albums_screen.dart
@@ -1,34 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_audio_query/flutter_audio_query.dart';
 import 'package:music_player/ui/atoms/album_tile.dart';
 import 'package:music_player/util/strings.dart';
-import 'package:music_player/viewmodels/pages/library_model.dart';
-import 'package:provider/provider.dart';
 
-class AlbumsScreen extends StatefulWidget {
-  AlbumsScreenState createState() => AlbumsScreenState();
-}
+class AlbumsScreen extends StatelessWidget {
+  final List<AlbumInfo> albums;
 
-class AlbumsScreenState extends State<AlbumsScreen> {
+  AlbumsScreen({@required this.albums});
+
   Widget build(BuildContext context) {
-    return Consumer<LibraryModel>(
-      builder: (context, model, state) => Padding(
-        padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 50.0),
-        child: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 2,
-          mainAxisSpacing: 2,
-          children: List.generate(
-            model.albumsInDevice.length,
-            (index) => AlbumTile(
-              onTap: () {
-                Navigator.of(context).pushNamed(ROUTE_ALBUM_PAGE);
-              },
-              albumTitle: model.albumsInDevice.elementAt(index).title,
-              artist: model.albumsInDevice.elementAt(index).artist,
-            ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 50.0),
+      child: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 2,
+        mainAxisSpacing: 2,
+        children: List.generate(
+          albums.length,
+          (index) => AlbumTile(
+            onTap: () {
+              Navigator.of(context).pushNamed(ROUTE_ALBUM_PAGE);
+            },
+            albumTitle: albums.elementAt(index).title,
+            artist: albums.elementAt(index).artist,
           ),
-          addAutomaticKeepAlives: false,
         ),
+        addAutomaticKeepAlives: false,
       ),
     );
   }
