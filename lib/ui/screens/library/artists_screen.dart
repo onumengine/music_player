08cfddf54b9821a030afefc3b840f54222d5bab4
onumdev <onumdev@gmail.com>
2021-07-18 22:02:57 +0100

    pass the artist ID and name to Navigator.pushNamed

--- lib/ui/screens/library/artists_screen.dart
+++ lib/ui/screens/library/artists_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_audio_query/flutter_audio_query.dart';
 import 'package:music_player/ui/atoms/artist_tile.dart';
+import 'package:music_player/util/strings.dart';
 
 class ArtistsScreen extends StatelessWidget {
   final List<ArtistInfo> artists;
@@ -17,6 +18,17 @@ class ArtistsScreen extends StatelessWidget {
           artists.length,
           (index) => ArtistTile(
             artistName: artists.elementAt(index).name,
+            onTap: () {
+              print("ABOUT TO OPEN THE ARTIST PAGE");
+              Navigator.of(context).pushNamed(
+                ROUTE_ARTIST_PAGE,
+                arguments: {
+                  "artistID": artists.elementAt(index).id,
+                  "artistName": artists.elementAt(index).name,
+                },
+              );
+              print("ARTIST PAGE OPENED");
+            },
           ),
         ),
       ),
