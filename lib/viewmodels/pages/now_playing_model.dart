6ca0e859bbe08ddacb04462d50017c82859442a9
onumdev <onumdev@gmail.com>
2021-07-25 12:50:56 +0100

    delete redundant methods in the NowPlayingModel

--- lib/viewmodels/pages/now_playing_model.dart
+++ lib/viewmodels/pages/now_playing_model.dart
@@ -1,16 +1,39 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:music_player/models/track.dart';
+import 'package:flutter_audio_query/flutter_audio_query.dart';
 import 'package:music_player/util/strings.dart';
+
+/// I'll save the following data to preferences on dispose of the
+/// [NowPlayingPage]:
+/// [currentlyPlayingTrackTitle]
+/// [currentlyPlayingQueue]
+/// [_trackProgress]
+/// This will enable a user to continue from where they stopped
+/// anytime they open the app
 
 class NowPlayingModel extends ChangeNotifier {
   bool playbackIsPlaying;
 
-  String _currentlyPlayingTrackTitle = '<unknown>';
-  String get currentlyPlayingTrackTitle => _currentlyPlayingTrackTitle;
-  set currentlyPlayingTrackTitle(String currentTrack) {
-    _currentlyPlayingTrackTitle = currentTrack;
+  SongInfo _currentlyPlayingSong;
+  SongInfo get currentlyPlayingSong => _currentlyPlayingSong;
+  set currentlyPlayingSong(SongInfo song) {
+    _currentlyPlayingSong = song;
+    notifyListeners();
+  }
+
+  List<SongInfo> _currentlyPlayingList;
+  List<SongInfo> get currentlyPlayingList => _currentlyPlayingList;
+  set currentlyPlayingList(List<SongInfo> list) {
+    _currentlyPlayingList = list;
+    notifyListeners();
+  }
+
+  /// I haven't commented this stub out because this variable is still being
+  /// used in the [NowPlayingScreen]
+  double _trackProgress = 0.0;
+  double get trackProgress => _trackProgress;
+  set trackProgress(double value) {
+    _trackProgress = value;
     notifyListeners();
   }
 
@@ -35,78 +58,10 @@ class NowPlayingModel extends ChangeNotifier {
     }
   }
 
-  String _currentlyPlayingTrackArtist = '<unknown>';
-  String get currentlyPlayingTrackArtist => _currentlyPlayingTrackArtist;
-  set currentlyPlayingTrackArtist(String currentArtist) {
-    _currentlyPlayingTrackArtist = currentArtist;
-    notifyListeners();
-  }
-
-  String _currentlyPlayingTrackAlbum = '<unknown>';
-  String get currentlyPlayingTrackAlbum => _currentlyPlayingTrackAlbum;
-  set currentlyPlayingTrackAlbum(String currentAlbum) {
-    _currentlyPlayingTrackAlbum = currentAlbum;
-    notifyListeners();
-  }
-
-  Image _currentlyPlayingAlbumArt;
-  Image get currentlyPlayingAlbumArt => _currentlyPlayingAlbumArt;
-  set currentlyPlayingAlbumArt(Image currentImage) {
-    _currentlyPlayingAlbumArt = currentImage;
-    notifyListeners();
-  }
-
-  DateTime _currentlyPlayingTrackDuration;
-  DateTime get currentlyPlayingTrackDuration => _currentlyPlayingTrackDuration;
-  set currentlyPlayingTrackDuration(DateTime currentTrackDuration) {
-    _currentlyPlayingTrackDuration = currentTrackDuration;
-    notifyListeners();
-  }
-
-  DateTime _currentlyPlayingElapsedTime;
-  DateTime get currentlyPlayingElapsedTime => _currentlyPlayingElapsedTime;
-  set currentlyPlayingElapsedTime(DateTime elapsedTime) {
-    _currentlyPlayingElapsedTime = elapsedTime;
-    notifyListeners();
-  }
-
-  double _trackProgress = 0.0;
-  double get trackProgress => _trackProgress;
-  set trackProgress(double value) {
-    _trackProgress = value;
-    notifyListeners();
-  }
-
-  int _currentlyPlayingTrackIndex;
-  int get currentlyPlayingTrackIndex => _currentlyPlayingTrackIndex;
-  set currentlyPlayingTrackIndex(int currentIndex) {
-    _currentlyPlayingTrackIndex = currentIndex;
-    notifyListeners();
-  }
-
-  int _songsInCurrentPlaylist;
-  int get songsInCurrentPlaylist => _songsInCurrentPlaylist;
-  set songsInCurrentPlaylist(int totalSongs) {
-    _songsInCurrentPlaylist = totalSongs;
-    notifyListeners();
-  }
-
-  List<Track> _musicLibrary;
-  List<Track> get musicLibrary => _musicLibrary;
-  set musicLibrary(List<Track> updatedLibrary) {
-    _musicLibrary = updatedLibrary;
-    notifyListeners();
-  }
-
   showSongActionsDialog() {}
 
   openArtistPage(BuildContext context) {
     Navigator.of(context).pushNamed(ROUTE_ARTIST_PAGE);
-  }
-
-  openEqualizerPage(BuildContext context) {
-    Navigator.pop(context);
-    Navigator.pushNamed(context, ROUTE_EQUALIZER_PAGE);
   }
 
   openAlbumPage(BuildContext context) {
